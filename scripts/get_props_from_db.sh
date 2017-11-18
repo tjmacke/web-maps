2f2a0701329053ec1fe43b5e29449c42eaaac5f5
Tom Macke <macke_tom@yahoo.com>
2017-11-18 14:42:29 -0800

    get_props_from_db.sh	-- wrapper to grab specified properties from sqlite file into tsv w/header file make_propfile.sh	-- convert file from above to pkey (tab) {json-obj} file for display by shp_to_geojson

---
+++ scripts/get_props_from_db.sh
@@ -0,0 +1,73 @@
+#! /bin/bash
+#
+. ~/etc/funcs.sh
+
+U_MSG="usage: $0 [ -help ] -db dbase { -q query | query-file }"
+
+DBASE=
+QUERY=
+QFILE=
+
+while [ $# -gt 0 ] ; do
+	case $1 in
+	-help)
+		echo "$U_MSG"
+		exit 0
+		;;
+	-db)
+		shift
+		if [ $# -eq 0 ] ; then
+			LOG ERROR "-db requires dbase argument"
+			echo "$U_USG" 1>&2
+			exit 1
+		fi
+		DBASE=$1
+		shift
+		;;
+	-q)
+		shift
+		if [ $# -eq 0 ] ; then
+			LOG ERROR "-q requires query argument"
+			echo "$U_MSG" 1>&2
+			exit 1
+		fi
+		QUERY="$1"
+		shift
+		;;
+	-*)
+		LOG ERROR "unknown option $1"
+		echo "$U_MSG" 1>&2
+		exit 1
+		;;
+	*)
+		QFILE=$1
+		shift
+		break
+		;;
+	esac
+done
+
+if [ $# -ne 0 ] ; then
+	LOG ERROR "extra arguments $*"
+	echo "$U_MSG" 1>&2
+	exit 1
+fi
+
+# TODO: allow for pfx as shape files have lots of related files
+if [ -z "$DBASE" ] ; then
+	LOG ERROR "missing -db dbase argument"
+	echo "$U_MSG" 1>&2
+	exit 1
+fi
+
+if [ ! -z "$QUERY" ] ; then
+	if [ ! -z "$QFILE" ] ; then
+		LOG ERROR "-q query not allowed with query-file"
+		echo "$U_MSG" 1>&2
+		exit 1
+	fi
+	echo "$QUERY"
+else
+	cat "$QFILE"
+fi	|\
+sqlite3 -separator '	' -cmd '.headers on' $DBASE
---
+++ scripts/make_propfile.sh
@@ -0,0 +1,121 @@
+#! /bin/bash
+#
+. ~/etc/funcs.sh
+
+U_MSG="usage; $0 [ -help ] -pk primary-key [ props-tsv-file ]"
+
+PKEY=
+FILE=
+
+while [ $# -gt 0 ] ; do
+	case $1 in
+	-help)
+		echo "$U_MSG"
+		exit 0
+		;;
+	-pk)
+		shift
+		if [ $# -eq 0 ] ; then
+			LOG ERROR "-pk requires primary-key argument"
+			echo "$U_MSG" 1>&2
+			exit 1
+		fi
+		PKEY=$1
+		shift
+		;;
+	-*)
+		LOG ERROR "unknown option $1"
+		echo "$U_MSG" 1>&2
+		exit 1
+		;;
+	*)
+		FILE=$1
+		shift
+		break
+		;;
+	esac
+done
+
+if [ $# -ne 0 ] ; then
+	LOG ERROR "extra arguments $*"
+	echo "$U_MSG" 1>&2
+	exit 1
+fi
+
+if [ -z "$PKEY" ] ; then
+	LOG ERROR "missing -pk primary-key argument"
+	echo "$U_MSG" 1>&2
+	exit 1
+fi
+
+awk -F'\t' 'BEGIN {
+	pkey = "'"$PKEY"'"
+}
+NR == 1 {
+	pk_fnum = 0
+	n_htab = 0
+	for(i = 1; i <= NF; i++){
+		if($i == pkey)
+			pk_fnum = i
+		n_htab++
+		htab[i] = $i
+	}
+	if(!pk_fnum){
+		printf("ERROR: main: primary key %s is not in list of fields\n", pkey) > "/dev/stderr"
+		err = 1;
+		exit err
+	}
+	next
+}
+{
+	printf("%d\t{", $pk_fnum)
+	for(i = 1; i <= NF; i++){
+		printf("\"%s\": %s%s", htab[i], mk_json_value($i), i < NF ? ", " : "")
+	}
+	printf("}\n")
+}
+END {
+	if(err)
+		exit err
+}
+function mk_json_value(str,   work, l_pfx) {
+
+	work = str
+	if(substr(work, 1, 1) == "-")
+		work = substr(work, 2)
+	# ints
+	if(work == "0")
+		return str
+	if(work ~ /^[1-9][0-9]*$/)
+		return str
+
+	# floats.  NB: digits requireed after .; only one 0 alloed before . or E
+	l_pfx = 0
+	if(match(work, /^0\.[0-9][0-9*]/))
+		l_pfx = RLENGTH
+	else if(match(work, /^[1-9][0-9]*\.[0-9][0-9]*/))
+		l_pfx = RLENGTH
+	else if(match(work, /^0[Ee]/))
+		l_pfx = RLENGTH - 1
+	else if(match(work, /^[1-9][0-9]*[Ee]/))
+		l_pfx = RLENGTH - 1
+
+	if(l_pfx > 0){
+		work = substr(work, l_pfx + 1)
+		if(work == "")
+			return str
+		if(work ~ /^[Ee]/){
+			work = substr(work, 2)
+			if(work ~ /^[+-]/)
+				work = substr(work, 2)
+			if(work ~ /^[0-9][0-9]*$/)
+				return str
+		}
+	}
+
+	work = str
+	gsub(/"/, "\\\"", work)
+
+	
+	return "\"" work "\""
+}' $FILE
