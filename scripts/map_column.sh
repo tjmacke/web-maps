e8304c5bf868f8f1d5081275a7f6e315730cad68
Tom Macke <macke_tom@yahoo.com>
2017-11-20 19:26:51 -0800

    map_column.sh		-- convert every value in a column into a new value.

---
+++ scripts/map_column.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+#
+. ~/etc/funcs.sh
+
+U_MSG="usage: $0 [ -help ] [ tsv-file ]"
+
+FILE=
+
+while [ $# -gt 0 ] ; do
+	case $1 in
+	-help)
+		echo "$U_MSG"
+		exit 0
+		;;
+	-*)
+		LOG ERROR "unknown option $1"
+		echo "$U_MSG" 1>&2
+		exit 1
+		;;
+	*)
+		FILE=$1
+		shift
+		break
+		;;
+	esac
+done
+
+if [ $# -ne 0 ] ; then
+	LOG ERROR "extra arguments $*"
+	echo "$U_MSG" 1>&2
+	exit 1
+fi
