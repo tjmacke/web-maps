1371f6a1e1e9fe383713779cf6287d00216cd27b
Tom Macke <macke_tom@yahoo.com>
2018-02-05 14:41:58 -0800

    color_graph.sh		-- 2 level color is working!

--- scripts/color_graph.sh
+++ scripts/color_graph.sh
@@ -78,25 +78,84 @@ fi
 sort -t $'\t' -k 3rn,3 $FILE	|\
 awk -F'\t' ' BEGIN {
 	verbose = "'"$VERBOSE"'" == "yes"
-	bcolor = "'"$BCOLOR"'"
+	bcolor = ("'"$BCOLOR"'" != "") ? "'"$BCOLOR"'" : "_NO_BCOLOR_"
 	id = "'"$ID"'"
-	pink = "#ff6290"
-	gold = "#ffbf00"
-	green = "#028900"
-	blue = "#7ba4dd"
-	purple = "#f3b4ff"
+
+	## BEGIN color defs
+	# I got these palettes from www.color-hex.com/color-palette/
+
+	# No bcolor [(uses pink, gold, green, blue, purple), 3]
+	all_colors["_NO_BCOLOR_", 1] = "#ff6290"
+	all_colors["_NO_BCOLOR_", 2] = "#ffbf00"
+	all_colors["_NO_BCOLOR_", 3] = "#028900"
+	all_colors["_NO_BCOLOR_", 4] = "#7ba4dd"
+	all_colors["_NO_BCOLOR_", 5] = "#f3b4ff"
+
+	# Princess Pink, 6653
+	all_colors["pink",        1] = "#ffc2cd"
+	all_colors["pink",        2] = "#ff93ac"
+	all_colors["pink",        3] = "#ff6289"
+	all_colors["pink",        4] = "#fc3468"
+	all_colors["pink",        5] = "#ff084a"
+	
+	# 24K GOLD, 2799
+	all_colors["gold",        1] = "#a67c00"
+	all_colors["gold",        2] = "#bf9b30"
+	all_colors["gold",        3] = "#ffbf00"
+	all_colors["gold",        4] = "#ffcf40"
+	all_colors["gold",        5] = "#ffdc73"
+
+	# I Loved in Shades of Green, 1325
+	all_colors["green",       1] = "#adff00"
+	all_colors["green",       2] = "#74d600"
+	all_colors["green",       3] = "#028900"
+	all_colors["green",       4] = "#00d27f"
+	all_colors["green",       5] = "#00ff83"
+
+	# 5 shades of blue, 44020
+	all_colors["blue",        1] = "#abbcea"
+	all_colors["blue",        2] = "#92b4e3"
+	all_colors["blue",        3] = "#7ba4dd"
+	all_colors["blue",        4] = "#628dd6"
+	all_colors["blue",        5] = "#4c74c9"
+
+	# shades of purple rain, 45572
+	all_colors["purple",      1] = "#fce4ff"
+	all_colors["purple",      2] = "#fad2ff"
+	all_colors["purple",      3] = "#f3b4ff"
+	all_colors["purple",      4] = "#ed8fff"
+	all_colors["purple",      5] = "#e24cff"
+	#
+	## END color defs
+
 	colors["n_colors"] = 5
-	# TODO: if bcolor != "", then 1,2,3,4,5 begome bcolor, bcolor+1, bcolor+2, bcolor-2, bcolor-1
-	colors["colors", 1] = pink
-	colors["colors", 2] = gold
-	colors["colors", 3] = green
-	colors["colors", 4] = blue
-	colors["colors", 5] = purple
-	colors["colors", pink] = 1
-	colors["colors", gold] = 2
-	colors["colors", green] = 3
-	colors["colors", blue] = 4
-	colors["colors", purple] = 5
+
+## BEGIN: orig
+#	colors["colors", 1] = pink
+#	colors["colors", 2] = gold
+#	colors["colors", 3] = green
+#	colors["colors", 4] = blue
+#	colors["colors", 5] = purple
+#	colors["colors", pink] = 1
+#	colors["colors", gold] = 2
+#	colors["colors", green] = 3
+#	colors["colors", blue] = 4
+#	colors["colors", purple] = 5
+## END: orig
+
+## BEGIN: new
+	colors["colors", 1] = c1 = all_colors[bcolor, 1]
+	colors["colors", 2] = c2 = all_colors[bcolor, 2]
+	colors["colors", 3] = c3 = all_colors[bcolor, 3]
+	colors["colors", 4] = c4 = all_colors[bcolor, 4]
+	colors["colors", 5] = c5 = all_colors[bcolor, 5]
+	colors["colors", c1] = 1
+	colors["colors", c2] = 2
+	colors["colors", c3] = 3
+	colors["colors", c4] = 4
+	colors["colors", c5] = 5
+## END: new
+
 	colors["used", 1] = 0
 	colors["used", 2] = 0
 	colors["used", 3] = 0
