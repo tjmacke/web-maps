6b580428bc4661b7d1daf3cf791eac5374f902fa
Tom Macke <macke_tom@yahoo.com>
2018-03-06 12:17:56 -0800

    find_parking.sh		-- mv sign data to data/Seattle_Parking, make colog config to inline

--- scripts/find_parking.sh
+++ scripts/find_parking.sh
@@ -9,7 +9,7 @@ if [ -z "$WM_HOME" ] ; then
 	exit 1
 fi
 WM_BIN=$WM_HOME/bin
-WM_DATA=$WM_HOME/work
+WM_DATA=$WM_HOME/data/Seattle_Parking
 
 if [ -z "$DM_HOME" ] ; then
 	LOG ERROR "DM_HOME not defined"
@@ -17,18 +17,24 @@ if [ -z "$DM_HOME" ] ; then
 fi
 DM_SCRIPTS=$DM_HOME/scripts
 
+# very simple config, unlikely to ever change, so just create it in line
+TMP_FP_CFILE=/tmp/fp_cfile.$$
+cat > $TMP_FP_CFILE <<_EOF_
+main.scale_type = factor
+main.values = 0.90,0.90,0.90 | 0.94,0.94,0.5 | 0.94,0.75,0.5 | 0.94,0.5,0.5
+main.keys = PPL | PLU | PCVL | PTRKL
+main.def_value = 0.63,0.63,0.94
+main.def_key_text = dest
+_EOF_
+
 TMP_OFILE=/tmp/out.$$
 TMP_EFILE=/tmp/err.$$
 TMP_PFILE=/tmp/pspots.$$
 TMP_CFILE=/tmp/cfile.$$
 TMP_SC_FILE=/tmp/sc_file.$$
 
-# TODO: parm?
-CFILE=$WM_HOME/work/fp.cfg
-
 GEO=
 ADDR=
-CFILE=$WM_HOME/work/fp.cfg
 FILE=
 
 while [ $# -gt 0 ] ; do
@@ -101,29 +107,27 @@ if [ $n_EFILE -ne 0 ] ; then
 	cat $TMP_EFILE 1>&2
 fi
 if [ $n_OFILE -ne 0 ] ; then
-	$WM_BIN/find_addrs_in_rect  -a $WM_DATA/sps_sorted.tsv $TMP_OFILE > $TMP_PFILE
+	$WM_BIN/find_addrs_in_rect -a $WM_DATA/sps_sorted.tsv $TMP_OFILE > $TMP_PFILE
 	awk -F'\t' '
 	@include "/Users/tom/work/dd_maps/lib/cfg_utils.awk"
 	@include "/Users/tom/work/dd_maps/lib/interp_utils.awk"
 	@include "/Users/tom/work/dd_maps/lib/color_utils.awk"
 	BEGIN {
-		cfile = "'"$CFILE"'"
+		cfile = "'"$TMP_FP_CFILE"'"
 		CFG_read(cfile, config)
-	#	CFG_dump("/dev/stderr", config)
 		
 		if(IU_init(config, color, "main")){
 			printf("ERROR: BEGIN: IU_init failed\n") > "/dev/stderr"
 			err = 1
 			exit err
 		}
-	#	IU_dump("/dev/stderr", color) 
 	}
 	{
 		iv = IU_interpolate(color, $NF)
 		printf("#%s\n", CU_rgb_to_24bit_color(iv))
 	}' $TMP_PFILE > $TMP_CFILE
-	$DM_SCRIPTS/cfg_to_json.sh $CFILE > $TMP_SC_FILE
+	$DM_SCRIPTS/cfg_to_json.sh $TMP_FP_CFILE > $TMP_SC_FILE
 	$DM_SCRIPTS/map_addrs.sh -sc $TMP_SC_FILE -cf $TMP_CFILE -at src $TMP_PFILE
 fi
 
-rm -f $TMP_OFILE $TMP_EFILE $TMP_PFILE $TMP_CFILE $TMP_SC_FILE
+rm -f $TMP_FP_CFILE $TMP_OFILE $TMP_EFILE $TMP_PFILE $TMP_CFILE $TMP_SC_FILE
