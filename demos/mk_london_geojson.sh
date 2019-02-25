70dac09a0b1b65af31ad0178ff4bc785368c4680
Tom Macke <macke_tom@yahoo.com>
2019-02-24 17:07:18 -0800

    Add london wards/subswards to demo

--- demos/mk_london_geojson.sh
+++ demos/mk_london_geojson.sh
@@ -2,7 +2,7 @@
 #
 . ~/etc/funcs.sh
 
-U_MSG="usage: $0 [ -help ] [ -b ] [ -sa adj-file ] [ -fmt F ] [ -h [ -d { union | lines | colors } ] ] (no arguments)"
+U_MSG="usage: $0 [ -help ] [ -b ] [ -trace ] [ -sa adj-file ] [ -h [ -d { union | lines | colors } ] (no arguments)"
 
 if [ -z "$WM_HOME" ] ; then
 	LOG ERROR "WM_HOME not defined"
@@ -12,16 +12,16 @@ WM_BIN=$WM_HOME/bin
 WM_BUILD=$WM_HOME/src
 WM_DATA=$WM_HOME/data
 WM_SCRIPTS=$WM_HOME/scripts
-SND_DATA=$WM_DATA/Seattle_Neighborhoods/WGS84
+LON_DATA=$WM_DATA/London-wards-2014
 
-TMP_PFILE=/tmp/sn.tsv.$$
-TMP_RNFILE=/tmp/sn.rnums.$$
-TMP_CFILE=/tmp/sn.colors.tsv.$$
-TMP_PFILE_2=/tmp/sn_2.tsv.$$
+TMP_PFILE=/tmp/lon.tsv.$$
+TMP_RNFILE=/tmp/lon.rnums.$$
+TMP_CFILE=/tmp/lon.colors.tsv.$$
+TMP_PFILE_2=/tmp/lon_2.tsv.$$
 
 USE_BUILD=
+TRACE=
 AFILE=
-FMT=
 HOPT=
 HDISP=
 
@@ -35,6 +35,10 @@ while [ $# -gt 0 ] ; do
 		USE_BUILD="yes"
 		shift
 		;;
+	-trace)
+		TRACE="yes"
+		shift
+		;;
 	-sa)
 		shift
 		if [ $# -eq 0 ] ; then
@@ -43,16 +47,6 @@ while [ $# -gt 0 ] ; do
 			exit 1
 		fi
 		AFILE=$1
-		shift
-		;;
-	-fmt)
-		shift
-		if [ $# -eq 0 ] ; then
-			LOG ERROR "-fmt requires format arg, one of wrap, plain, list"
-			echo "$U_MSG" 1>&2
-			exit 1
-		fi
-		FMT=$1
 		shift
 		;;
 	-h)
@@ -76,22 +70,18 @@ while [ $# -gt 0 ] ; do
 		;;
 	*)
 		LOG ERROR "extra arguments $*"
-		echo "$U_MSG" 1>&2
-		exit 1
+		shift
+		break
 		;;
 	esac
 done
 
 if [ "$USE_BUILD" == "yes" ] ; then
 	BINDIR=$WM_BUILD
-	BOPT="-b"
+	BOPT=-b
 else
 	BINDIR=$WM_BIN
 	BOPT=
-fi
-
-if [ ! -z "$FMT" ] ; then
-	FMT="-fmt $FMT"
 fi
 
 if [ "$HOPT" == "yes" ] ; then
@@ -107,25 +97,29 @@ if [ "$HOPT" == "yes" ] ; then
 	elif [ "$HDISP" == "colors" ] ; then
 		SAVE_BC="yes"
 	elif [ "$HDISP" != "lines" ] ; then
-		LOG ERROR "bad hierarchy display type $HDISP, must union, lines or colors"
+		LOG ERROR "bad hierarchy display type $HDISP, must be union, lines or colors"
 		echo "$U_MSG" 1>&2
 		exit 1
 	fi
 elif [ ! -z "$HDISP" ] ; then
-	LOG ERROR "-d option requires -h option"
+	LOG ERROR "-d requires -h option"
 	echo "$U_MSG" 1>&2
-	exit 1
+	exite 1
 else
 	ID="title"
 	MK="title"
 	PFX=
 fi
 
-# 1. select the neighborhoods. Currently neighborhoods w/L_HOOD = "" are skipped
-sqlite3 $SND_DATA/Neighborhoods.db <<_EOF_ |
+if [ ! -z "$TRACE" ] ; then
+	TRACE="-trace"
+fi
+
+# 1. select the wards (neighborhoods?)
+sqlite3 $LON_DATA/london.db <<_EOF_	|
 .headers on
-.mode tabs
-select OBJECTID, L_HOOD, S_HOOD from data order by L_HOOD, S_HOOD ;
+.mode tab
+select rnum, NAME, BOROUGH from data ;
 _EOF_
 awk -F'\t' 'BEGIN {
 	hopt = "'"$HOPT"'" == "yes"
@@ -142,8 +136,6 @@ NR > 1 {
 END {
 	for(i = 1; i <= n_recs; i++){
 		titles[i] = title = mk_title(fnums, recs[i])
-		if(title == "")
-			continue;
  		t_count[title]++
 		if(hopt){
 			ids[i] = id = mk_id(fnums, recs[i])
@@ -161,14 +153,14 @@ END {
 		nf = split(recs[i], ary)
 		if(t_count[title] > 1){
 			if(title !~ /\/$/)
-				title = title "_" ary[fnums["OBJECTID"]]
+				title = title "_" ary[fnums["rnum"]]
 		}
-		printf("%s\t%s", ary[fnums["OBJECTID"]], title)
+		printf("%s\t%s", ary[fnums["rnum"]], title)
 		if(hopt){
 			id = mk_id(fnums, recs[i])
 			if(i_count[id] > 1){
 				if(id !~ /\/$/)
-					id = id "_" ary[fnums["OBJECTID"]]
+					id = id "_" ary[fnums["rnum"]]
 			}
 			printf("\t%s", id)
 		}
@@ -178,41 +170,32 @@ END {
 function mk_title(fnums, rec,   nf, ary, i, title) {
 
 	nf = split(rec, ary)
-	if(ary[fnums["L_HOOD"]] == "")
-		title = ""
-	else if(ary[fnums["L_HOOD"]] == "NO BROADER TERM")
-		title = ary[fnums["S_HOOD"]]
-	else
-		title = sprintf("%s/%s", ary[fnums["L_HOOD"]], ary[fnums["S_HOOD"]])
+	title = sprintf("%s/%s", ary[fnums["BOROUGH"]], ary[fnums["NAME"]])
 	return title
 }
 function mk_id(fnums, rec,   nf, ary, i, id) {
 
 	nf = split(rec, ary)
-	if(ary[fnums["L_HOOD"]] == "")
-		id = ""
-	else if(ary[fnums["L_HOOD"]] == "NO BROADER TERM")
-		id = ary[fnums["S_HOOD"]] 
-	else
-		id = sprintf("%s/", ary[fnums["L_HOOD"]])
+	id = sprintf("%s/", ary[fnums["BOROUGH"]])
 	return id
 }' > $TMP_PFILE
-tail -n +2 $TMP_PFILE | awk '{ print $1 }'						> $TMP_RNFILE
-$BINDIR/shp_to_geojson -sf $SND_DATA/Neighborhoods -pf $TMP_PFILE -pk rnum $TMP_RNFILE	|\
-$WM_SCRIPTS/find_adjacent_polys.sh -fmt wrapped -id $ID					|\
-$WM_SCRIPTS/rm_dup_islands.sh								|\
+tail -n +2 $TMP_PFILE | awk '{ print $1 }'					> $TMP_RNFILE
+$BINDIR/shp_to_geojson -sf $LON_DATA/london -pf $TMP_PFILE -pk rnum $TMP_RNFILE	|\
+$WM_SCRIPTS/find_adjacent_polys.sh $TRACE -fmt wrapped -id $ID			|\
+$WM_SCRIPTS/rm_dup_islands.sh							|\
 if [ ! -z "$AFILE" ] ; then
 	tee $AFILE
 else
 	cat
-fi											|\
-$WM_SCRIPTS/color_graph.sh -id $ID 							> $TMP_CFILE
+fi									|\
+$WM_SCRIPTS/color_graph.sh $TRACE -id $ID				> $TMP_CFILE
 if [ -z "$SAVE_BC" ] ; then
-	$WM_SCRIPTS/add_columns.sh -mk $MK $PFX $TMP_PFILE $TMP_CFILE 			> $TMP_PFILE_2
-	$BINDIR/shp_to_geojson -sf $SND_DATA/Neighborhoods -pf $TMP_PFILE_2 -pk rnum $FMT $TMP_RNFILE
+	$WM_SCRIPTS/add_columns.sh $TRACE -mk $MK $PFX $TMP_PFILE $TMP_CFILE	> $TMP_PFILE_2
+	$BINDIR/shp_to_geojson -sf $LON_DATA/london -pf $TMP_PFILE_2 -pk rnum $TMP_RNFILE
 else
-	$WM_SCRIPTS/add_columns.sh -mk $MK $PFX $TMP_PFILE $TMP_CFILE	|\
-	$WM_SCRIPTS/make_2l_colors.sh $BOPT $FMT -sf $SND_DATA/Neighborhoods -id id
+#	$WM_SCRIPTS/add_columns.sh $TRACE -mk $MK $PFX $TMP_PFILE $TMP_CFILE | tee lon_2.colors.tsv	> $TMP_PFILE_2
+	$WM_SCRIPTS/add_columns.sh $TRACE -mk $MK $PFX $TMP_PFILE $TMP_CFILE |\
+	$WM_SCRIPTS/make_2l_colors.sh $BOPT -sf $LON_DATA/london -id id
 fi
 
 rm -f $TMP_PFILE $TMP_RNFILE $TMP_CFILE $TMP_PFILE_2
