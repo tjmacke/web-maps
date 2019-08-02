69df109925506db8d9b75c0acd1c600ca7c9ee1e
Tom Macke <macke_tom@yahoo.com>
2019-08-02 09:30:25 -0700

    mk_*_geojson.sh now use map_shapes.sh

--- demos/mk_london_geojson.sh
+++ demos/mk_london_geojson.sh
@@ -2,63 +2,26 @@
 #
 . ~/etc/funcs.sh
 
-U_MSG="usage: $0 [ -help ] [ -b ] [ -trace ] [ -sa adj-file ] [ -fmt F ] [ -h [ -d { lines*|colors|union } ] ] shape-file"
+U_MSG="usage: $0 [ -help ] [ -h [ -d { lines*|colors|union } ] ] (no arguments)"
 
 if [ -z "$WM_HOME" ] ; then
 	LOG ERROR "WM_HOME not defined"
 	exit 1
 fi
-WM_BIN=$WM_HOME/bin
-WM_BUILD=$WM_HOME/src
 WM_DATA=$WM_HOME/data
+WM_DEMOS=$WM_HOME/demos
 WM_SCRIPTS=$WM_HOME/scripts
 
-TMP_PFILE=/tmp/pfile.$$
-TMP_RNFILE=/tmp/rnums.$$
-TMP_CFILE=/tmp/colors.$$
-TMP_PFILE_2=/tmp/pfile_2.$$
-
-USE_BUILD=
-TRACE=
-AFILE=
-FMT=
 HOPT=
 HDISP=
-SHP_FILE=
+
+SHP_ROOT=$WM_DATA/London-wards-2014/london
 
 while [ $# -gt 0 ] ; do
 	case $1 in
 	-help)
 		echo "$U_MSG"
 		exit 0
-		;;
-	-b)
-		USE_BUILD="yes"
-		shift
-		;;
-	-trace)
-		TRACE="yes"
-		shift
-		;;
-	-sa)
-		shift
-		if [ $# -eq 0 ] ; then
-			LOG ERROR "-sa requires adj-file argument"
-			echo "$U_MSG" 1>&2
-			exit 1
-		fi
-		AFILE=$1
-		shift
-		;;
-	-fmt)
-		shift
-		if [ $# -eq 0 ] ; then
-			LOG ERROR "-fmt requires format arg, one of wrap, plain, list"
-			echo "$U_MSG" 1>&2
-			exit 1
-		fi
-		FMT=$1
-		shift
 		;;
 	-h)
 		HOPT="yes"
@@ -80,50 +43,22 @@ while [ $# -gt 0 ] ; do
 		exit 1
 		;;
 	*)
-		SHP_FILE=$1
-		shift
-		break
+		LOG ERROR "extra arguments $*"
+		echo "$U_MSG" 1>&2
+		exit 1
 		;;
 	esac
 done
 
-if [ $# -ne 0 ] ; then
-	LOG ERROR "extra arguments $*"
-	echo "$U_MSG" 1>&2
-	exit 1
-fi
-
-if [ -z "$SHP_FILE" ] ; then
-	LOG ERROR "missing shape-file argument"
-	echo "$U_MSG" 1>&2
-	exit 1
-fi
-SHP_ROOT="$(echo $SHP_FILE | awk -F'.' '{ r = $1 ; for(i = 2; i < NF; i++) { r = r "." $i } ; print r }')"
-
-if [ "$USE_BUILD" == "yes" ] ; then
-	BINDIR=$WM_BUILD
-	BOPT="-b"
-else
-	BINDIR=$WM_BIN
-	BOPT=
-fi
-
-if [ ! -z "$FMT" ] ; then
-	FMT="-fmt $FMT"
-fi
-
 if [ "$HOPT" == "yes" ] ; then
-	ID="id"
-	MK="id"
-	PFX="-pfx_of title"
-	SAVE_BC=
+	HOPT="-h"
 	if [ -z "$HDISP" ] ; then
-		HDISP="lines"
+		HDISP="-d lines"
 	elif [ "$HDISP" == "union" ] ; then
 		LOG ERROR "-d union not yet implemented, use lines or colors"
 		exit 1
 	elif [ "$HDISP" == "colors" ] ; then
-		SAVE_BC="yes"
+		HDISP="-d colors"
 	elif [ "$HDISP" != "lines" ] ; then
 		LOG ERROR "bad hierarchy display type $HDISP, must be one of union, lines or colors"
 		echo "$U_MSG" 1>&2
@@ -133,95 +68,8 @@ elif [ ! -z "$HDISP" ] ; then
 	LOG ERROR "-d requires -h option"
 	echo "$U_MSG" 1>&2
 	exit 1
-else
-	ID="title"
-	MK="title"
-	PFX=
 fi
 
-if [ ! -z "$TRACE" ] ; then
-	TRACE="-trace"
-fi
+$WM_SCRIPTS/map_shapes.sh $HOPT $HDISP -sel $WM_DEMOS/select_lw.sh $SHP_ROOT
 
-# 1. select the shapes
-sqlite3 $SHP_ROOT.db <<_EOF_	|
-.headers on
-.mode tabs
-select rnum, NAME, BOROUGH from data ;
-_EOF_
-awk -F'\t' 'BEGIN {
-	hopt = "'"$HOPT"'" == "yes"
-}
-NR == 1 {
-	for(i = 1; i <= NF; i++)
-		fnums[$i] = i
-}
-NR > 1 {
-	n_recs++
-	recs[n_recs] = $0
-}
-END {
-	for(i = 1; i <= n_recs; i++){
-		titles[i] = title = mk_title(fnums, recs[i])
- 		t_count[title]++
-		if(hopt){
-			ids[i] = id = mk_id(fnums, recs[i])
-			i_count[id]++
-		}
-	}
-	printf("%s\t%s", "rnum", "title")
-	if(hopt)
-		printf("\t%s", "id")
-	printf("\n")
-	for(i = 1; i <= n_recs; i++){
-		title = titles[i]
-		if(title == "")
-			continue
-		nf = split(recs[i], ary)
-		if(t_count[title] > 1){
-			if(title !~ /\/$/)
-				title = title "_" ary[fnums["rnum"]]
-		}
-		printf("%s\t%s", ary[fnums["rnum"]], title)
-		if(hopt){
-			id = mk_id(fnums, recs[i])
-			if(i_count[id] > 1){
-				if(id !~ /\/$/)
-					id = id "_" ary[fnums["rnum"]]
-			}
-			printf("\t%s", id)
-		}
-		printf("\n")
-	}
-}
-function mk_title(fnums, rec,   nf, ary, i, title) {
-
-	nf = split(rec, ary)
-	title = sprintf("%s/%s", ary[fnums["BOROUGH"]], ary[fnums["NAME"]])
-	return title
-}
-function mk_id(fnums, rec,   nf, ary, i, id) {
-
-	nf = split(rec, ary)
-	id = sprintf("%s/", ary[fnums["BOROUGH"]])
-	return id
-}' > $TMP_PFILE
-tail -n +2 $TMP_PFILE | awk '{ print $1 }'					> $TMP_RNFILE
-$BINDIR/shp_to_geojson -sf $SHP_ROOT -pf $TMP_PFILE -pk rnum $TMP_RNFILE	|
-$WM_SCRIPTS/find_adjacent_polys.sh $TRACE -fmt wrapped -id $ID			|
-$WM_SCRIPTS/rm_dup_islands.sh							|
-if [ ! -z "$AFILE" ] ; then
-	tee $AFILE
-else
-	cat
-fi										|
-$WM_SCRIPTS/color_graph.sh $TRACE -id $ID					> $TMP_CFILE
-if [ -z "$SAVE_BC" ] ; then
-	$WM_SCRIPTS/add_columns.sh $TRACE -mk $MK $PFX $TMP_PFILE $TMP_CFILE	> $TMP_PFILE_2
-else
-	$WM_SCRIPTS/add_columns.sh $TRACE -mk $MK $PFX $TMP_PFILE $TMP_CFILE	|
-	$WM_SCRIPTS/make_2l_colors.sh $BOPT $FMT -sf $SHP_ROOT -id id		> $TMP_PFILE_2
-fi
-$BINDIR/shp_to_geojson -sf $SHP_ROOT -pf $TMP_PFILE_2 -pk rnum $FMT $TMP_RNFILE
-
-rm -f $TMP_PFILE $TMP_RNFILE $TMP_CFILE $TMP_PFILE_2
+exit 0
--- demos/mk_paris_geojson.sh
+++ demos/mk_paris_geojson.sh
@@ -2,45 +2,23 @@
 #
 . ~/etc/funcs.sh
 
-U_MSG="usage: $0 [ -help ] [ -b ] [ -sa adj-file ] (no arguments)"
+U_MSG="usage: $0 [ -help ] (no arguments)"
 
 if [ -z "$WM_HOME" ] ; then
 	LOG ERROR "WM_HOME not defined"
 	exit 1
 fi
-WM_BIN=$WM_HOME/bin
-WM_BUILD=$WM_HOME/src
 WM_DATA=$WM_HOME/data
+WM_DEMOS=$WM_HOME/demos
 WM_SCRIPTS=$WM_HOME/scripts
-PAR_DATA=$WM_DATA/Paris
 
-TMP_PFILE=/tmp/par.tsv.$$
-TMP_RNFILE=/tmp/par.rnums.$$
-TMP_CFILE=/tmp/par.colors.tsv.$$
-TMP_PFILE_2=/tmp/par_2.tsv.$$
-
-USE_BUILD=
-AFILE=
+SHP_ROOT=$WM_DATA/Paris/arrondissements
 
 while [ $# -gt 0 ] ; do
 	case $1 in
 	-help)
 		echo "$U_MSG"
 		exit 0
-		;;
-	-b)
-		USE_BUILD="yes"
-		shift
-		;;
-	-sa)
-		shift
-		if [ $# -eq 0 ] ; then
-			LOG ERROR "-sa requires adj-file argument"
-			echo "$U_MSG" 1>&2
-			exit 1
-		fi
-		AFILE=$1
-		shift
 		;;
 	-*)
 		LOG ERROR "unknown option $1"
@@ -55,46 +33,6 @@ while [ $# -gt 0 ] ; do
 	esac
 done
 
-if [ "$USE_BUILD" == "yes" ] ; then
-	BINDIR=$WM_BUILD
-else
-	BINDIR=$WM_BIN
-fi
+$WM_SCRIPTS/map_shapes.sh -sel $WM_DEMOS/select_paris.sh $SHP_ROOT
 
-# 1. select the data
-sqlite3 $PAR_DATA/arrondissements.db <<_EOF_ |
-.headers on
-.mode tab
-select rnum, l_ar as ar_num, l_aroff as ar_name from data ;
-_EOF_
-awk -F'\t' 'NR == 1 {
-	for(i = 1; i <= NF; i++)
-		fnums[$i] = i
-}
-NR > 1 {
-	n_recs++
-	recs[n_recs] = $0
-}
-END {
-	printf("%s\t%s\n", "rnum", "title")
-	for(i = 1; i <= n_recs; i++)
-		printf("%d\t%s\n", i, mk_title(fnums, recs[i]))
-}
-function mk_title(fnums, rec,   n_ary, ary, n_ary2, ary2) {
-	n_ary = split(rec, ary)
-	n_ary2 = split(ary[fnums["ar_num"]], ary2, /  */)
-	return sprintf("%-6s %s", (ary2[1] ":"), ary[fnums["ar_name"]])
-}' > $TMP_PFILE
-tail -n +2 $TMP_PFILE | awk '{ print $1 }' > $TMP_RNFILE
-$BINDIR/shp_to_geojson -sf $PAR_DATA/arrondissements -pf $TMP_PFILE -pk rnum $TMP_RNFILE	|
-$WM_SCRIPTS/find_adjacent_polys.sh -fmt wrapped -id title					|
-$WM_SCRIPTS/rm_dup_islands.sh									|
-if [ ! -z "$AFILE" ] ; then
-	tee $AFILE
-else
-	cat
-fi												|
-$WM_SCRIPTS/color_graph.sh -id title								> $TMP_CFILE
-$WM_SCRIPTS/add_columns.sh -mk title $TMP_PFILE $TMP_CFILE					> $TMP_PFILE_2
-$BINDIR/shp_to_geojson -sf $PAR_DATA/arrondissements -pf $TMP_PFILE_2 -pk rnum $TMP_RNFILE
-rm -rf $TMP_PFILE $TMP_RNFILE $TMP_CFILE $TMP_PFILE_2
+exit 1
